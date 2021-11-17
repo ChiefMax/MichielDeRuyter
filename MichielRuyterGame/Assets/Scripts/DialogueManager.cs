aafb1f9b2cc0b0f50590cc03d2d4250561d84534
Max <maxmanenschijn@gmail.com>
2021-11-17 15:02:00 +0100

    Images
    Added way to change the picture with the script along with the text

--- MichielRuyterGame/Assets/Scripts/DialogueManager.cs
+++ MichielRuyterGame/Assets/Scripts/DialogueManager.cs
@@ -9,6 +9,9 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text nameText;
     public TMP_Text dialogueText;
 
+    public RawImage imageToDisplay;
+    public Queue<Texture> rawImages = new Queue<Texture>();
+
     private Queue<string> sentences;
 
     // Start is called before the first frame update
@@ -17,16 +20,22 @@ public class DialogueManager : MonoBehaviour
         sentences = new Queue<string>();
     }
 
-    public void StartDialogue(Dialogue dialogue)
+    public void StartDialogue(Dialogue dialogue, ImagesShow show)
     {
-        Debug.Log("Starting convo with " + dialogue.name);
+        //Debug.Log("Starting convo with " + dialogue.name);
         nameText.text = dialogue.name;
 
         sentences.Clear();
+        rawImages.Clear();
 
         foreach (string sentence in dialogue.sentences) 
         {
             sentences.Enqueue(sentence);
+        }
+
+        foreach ( Texture texture in show.textures) 
+        {
+            rawImages.Enqueue(texture);
         }
 
         DisplayNextSentence();
@@ -40,9 +49,21 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
+
+
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        Texture texture = rawImages.Dequeue();
+        //Debug.Log(sentence);
         dialogueText.text = sentence;
+        imageToDisplay.texture = texture;
+        //foreach (var image in rawImages)
+        //{
+        //    if (sentences.Count == rawImages.Count)
+        //    {
+        //        Debug.Log("sentences count: " + sentences.Count + "raw image texture count: " + rawImages.Count);
+        //        imageToDisplay.texture = image;
+        //    }
+        //}
     }
 
     void EndDialogue() 
